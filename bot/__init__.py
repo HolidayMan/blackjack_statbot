7d4b1da72d3393719321b782b2648b1fe0391564
HolidayMan <lesha.pitenko791@gmail.com>
2020-04-29 18:56:23 +0300

    added basic django configuration

---
+++ bot/__init__.py
@@ -0,0 +1,5 @@
+import telebot
+from blackjack_statbot.settings import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
---
+++ prod_settings.py
@@ -0,0 +1,16 @@
+import os
+from blackjack_statbot.settings import BASE_DIR
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+TOKEN = "<your token>"
+BOT_ID = TOKEN.split(':')[0]
+
+SECRET_KEY = '<your SECRET_KEY>'  # django SECRET_KEY
+
+DOMAIN = 'my_domain'
