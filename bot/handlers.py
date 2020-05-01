a6977d3191988f1c4e490ec97579ae4d2d2b8cef
Kostiantyn <ostap99.2002@gmail.com>
2020-05-01 21:43:09 +0300

    completed commands

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,45 +1,44 @@
-from bot import BOT
+import telebot
 import bot.phrases as ph
 
+from bot import BOT
 
-class channel:
-    def __init__ (self, name):
-        self.name = name
-        chat.id = chat_id
-        user.id = user_id
+API_TOKEN = '<1204011428:AAEObvwZgLWKMCm5EbEnJBQmz6MqIP3ui_I>'
+
+bot = telebot.TeleBot(API_TOKEN)
+#я знаю что мы уже указывали его. Оно не хочет работать
+
+user_dict = {}
+
+
+class Channel:
+    def __init__ (self, username, chat_id, user_id):
+        username = username
+        chat_id = chat_id
+        user_id = user_id
         
 @BOT.message_handler(comands = ['addchanel'])
 def add_channel(message): 
-    msg = BOT.send_message(message, ph.ADD_CHANNEL
-    bot.register_next_step_handler(msg, process_name_step)
+    msg = BOT.send_message(message.chat.id, ph.ADD_CHANNEL_MESSAGE
+    bot.register_next_step_handler(msg, getChatMember)
     
     
-def get_channel_id(message):
-    try:getChatMember(user,chat_id, user_id)  
-        user = User(name)
-        user_dict[chat_id] = user
-        #user_dict[chat_id] = user
-        chat_id = message.chat.id
-    except Exception: bot.reply_to(message, ph.NOT_ADMIN_MESSAGE)
-    #channel_record = MyModelName(my_field_name="Instance #1")
-    #channel_record.save
-    #bot.reply_to(message, ph.SUCCESSFULLY_ADDED_MESSAGE)
+def getChatMember(user,chat_id, user_id):
+    try:
+        TgChannel.objects.create(tg_id=tg_id, first_name=first_name, username=username)
+        bot.reply_to(message, ph.SUCCESSFULLY_ADDED_MESSAGE)
+    except Exception:
+        bot.reply_to(message.chat.id, ph.NOT_ADMIN_MESSAGE)
   
               
 @BOT.message_handler(commands = ['delete_channel'])
 def delete_channel_by_id(message):
-    try:getChatMember(user,chat_id, user_id)  
-        user = User(name)
-        user_dict[chat_id] = user
-        #user_dict[chat_id] = user
-        chat_id = message.chat.id
-    except Exception: bot.reply_to(message, ph.NO_ADDED_CHANNELS_MESSAGE)
-    #channel_record.filter(id=chat_id).delete()
+    try:
+        TgChannel.objects(tg_id=tg_id, username=username).delete()
+    except Exception:
+        BOT.reply_to(message, ph.NO_ADDED_CHANNELS_MESSAGE)
 
-
-#exept Exception - временная мера, испрввлю, просто надо же что-то отлавливать. А что именно, я не ебу
-#код писался с мобилы, я просто пытался понять что я хочу от бота, ну и от жизни в целом
-#исправлю, доработаю 
+#exept Exception - вынужденная мера, испрввлю, просто надо же что-то отлавливать. А что именно, я не ебу
 
 
 
