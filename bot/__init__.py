1087bf6926e96df5ac0b80385deaa5afc176be25
HolidayMan <lesha.pitenko791@gmail.com>
2020-05-02 13:26:49 +0300

    configured logging

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,5 +1,12 @@
+import logging
+
 import telebot
-from blackjack_statbot.settings import TOKEN
+from blackjack_statbot.settings import TOKEN, LOG_FILE
 
 BOT = telebot.TeleBot(TOKEN)
 
+logger = telebot.logger
+logging.basicConfig(filename=LOG_FILE, filemode='a', format='%(asctime)s:%(name)s - %(message)s')
+
+BOT.enable_save_next_step_handlers(delay=1)
+BOT.load_next_step_handlers()
